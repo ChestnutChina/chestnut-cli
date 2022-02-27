888a45c4cb38f9ce641977c92fd7c69fcd21e333
evan <454002679@qq.com>
2022-02-27 14:45:28 +0800

    ci: 修改template下文件的内容

--- template/config/.eslintrc.js
+++ template/config/.eslintrc.js
@@ -32,20 +32,19 @@ module.exports = defineConfig({
     quotes: [2, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     'quote-props': 'off',
     'no-shadow': ['error'],
-    // 关闭冲突规则
     'prettier/prettier': [
       'error',
       {
-        semi: true, // 句尾添加分号
-        tabWidth: 2, // 缩进字节数
-        printWidth: 120, // 超过最大值换行
-        endOfLine: 'auto', // 结尾是 \n \r \n\r auto
-        singleQuote: true, // 使用单引号代替双引号
-        arrowParens: 'avoid', // (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
-        trailingComma: 'all', // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
-        bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
-        proseWrap: 'never', // 不使用。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
-        htmlWhitespaceSensitivity: 'ignore', // 包裹文字时候结束标签的结尾尖括号掉到了下一行
+        semi: true,
+        tabWidth: 2,
+        printWidth: 120,
+        endOfLine: 'auto',
+        singleQuote: true,
+        arrowParens: 'avoid',
+        trailingComma: 'all',
+        bracketSpacing: true,
+        proseWrap: 'never',
+        htmlWhitespaceSensitivity: 'ignore',
       },
       {
         usePrettierrc: false,
