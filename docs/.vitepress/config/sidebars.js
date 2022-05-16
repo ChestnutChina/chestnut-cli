4cd21ef190818c91766cc40e9ef6b634e1bd8ec0
evan <454002679@qq.com>
2022-05-17 02:12:46 +0800

    feat: 总体概要

--- docs/.vitepress/config/sidebars.js
+++ docs/.vitepress/config/sidebars.js
@@ -1,13 +1,16 @@
 import guideLocal from '../json/guide.json';
 import featureLocal from '../json/feature.json';
+import webglLocal from '../json/webgl.json';
 
 const getGuide = () => guideLocal;
 const getFeature = () => featureLocal;
+const getWebGL = () => webglLocal;
 
 const getSidebars = () => {
   return {
     "guide": getGuide(),
     "feature": getFeature(),
+    "webgl": getWebGL(),
   };
 };
 
